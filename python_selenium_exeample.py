41327ba36ec94dce7509069ed2732efbbb290625
Jarod <elv-komivi.assm@eduge.ch>
2020-12-08 10:16:22 +0100

    Selenium quelque exemple

--- python_selenium_exeample.py
+++ python_selenium_exeample.py
@@ -1,34 +1,11 @@
-from time import sleep
-
 from selenium import webdriver
+
 
 def OuverturedeUrl():
     driver = webdriver.Chrome(executable_path='Driver\chromedriver.exe')
+    driver.get('https://esig-sandbox.ch/team20_5_v2/')
+    driver.maximize_window()
 
-    # La fenetre prend la totalité de l'écran
-    #driver.maximize_window()
-
-    driver.get('http://www.seleniumeasy.com/test/basic-first-form-demo.html')
-
-    #
-    # assert "Selenium Easy Demo - Simple Form to Automate using Selenium" in driver.title
-    #
-    # eleUserMessage = driver.find_element_by_id("user-message")
-    #
-    # eleUserMessage.clear()
-    #
-    # eleUserMessage.send_keys("Test Python")
-    #
-    # eleShowMsgBtn = driver.find_element_by_css_selector('#get-input > .btn')
-    #
-    # eleShowMsgBtn.click()
-    #
-    # eleYourMsg = driver.find_element_by_id("display")
-    #
-    # assert "Test Python" in eleYourMsg.text
-
-    sleep(9999999999999)
-    driver.close()
 
 
 if __name__ == '__main__':
